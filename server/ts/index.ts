d1fdc4fe5701022ca218846e631b9bbe449232f1
Marek Sklář <99105387+MarekSklar@users.noreply.github.com>
2024-04-24 09:09:39 +0200

    Electron setup

--- server/ts/index.ts
+++ server/ts/index.ts
@@ -1,1 +1,46 @@
-console.log("Test!");
+import { app, BrowserWindow, ipcMain } from 'electron';
+import path from 'path';
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) {
+    app.quit();
+}
+
+const createWindow = () => {
+    // Create the browser window.
+    const mainWindow = new BrowserWindow({
+        width: 1250,
+        height: 770,
+        webPreferences: {
+            preload: path.join(__dirname, '/server/js/preload/preload.js')
+        },
+    });
+
+    // and load the index.html of the app.
+    mainWindow.loadFile(path.join(__dirname + '/client/html/index.html'));
+
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools();
+};
+
+app.on('ready', () => {
+    createWindow();
+
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('before-quit', () => {
+    // Clear in-memory storage or session storage
+    // Your logic to clear data goes here
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
